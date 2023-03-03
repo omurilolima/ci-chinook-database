ac1d632558d2f2a94cbd272ed78038fbdea7f24c
Murilo Lima <msl@cesar.school>
2023-03-03 18:17:44 +0000

    Class-based models

--- sql-expression.py
+++ sql-expression.py
@@ -49,7 +49,8 @@ with db.connect() as connection:
     # ).with_only_columns([artist_table.c.Name])
 
     # Query 3 - select only "Queen" from the "Artist" table
-    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+    # select_query = artist_table.select(
+    # ).where(artist_table.c.Name == "Queen")
 
     # Query 4 - select only by "ArtistId" #51 from the "Artist" table
     # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
@@ -59,8 +60,9 @@ with db.connect() as connection:
 
     # Query 6 - select all tracks where the composer is
     # "Queen" from the "Track" table
-    select_query = track_table.select().where(track_table.c.Composer == "Queen")
-    
+    select_query = track_table.select(
+    ).where(track_table.c.Composer == "Queen")
+
     results = connection.execute(select_query)
     for result in results:
         print(result)
